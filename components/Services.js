23a57cc25303d8f875d347a9cd660014ee3d98da
sayan2809 <dassayan966@gmail.com>
2023-10-27 13:57:03 +0530

    fix services

--- components/Services.js
+++ components/Services.js
@@ -1,49 +1,82 @@
 export default function Services() {
-    return(
-        <div className=" flex flex-col justify-center items-center px-[120px] mt-20">
-            <h1 className="text-[40px]">It&#39;s time to try our Services</h1>
-            <div className="grid grid-cols-4 gap-5 mt-16">
-                <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
-                    <h1 className=" font-[540] text-[18px] text-center">HOSPITALITY<br/> COURIERS</h1>
-                    <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">We give you hours of your precious life back, with full price transparency, by supplying everything you need to run your business from our fulfillment centers to your shelves and fridges.</p>
-                    <a
-                        className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
-                        href={"/"}
-                    >
-                        Order Delivery
-                    </a>
-                </div>
-                <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
-                    <h1 className=" font-[540] text-[18px] text-center">HOSPITALITY<br/> COURIERS</h1>
-                    <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">We give you hours of your precious life back, with full price transparency, by supplying everything you need to run your business from our fulfillment centers to your shelves and fridges.</p>
-                    <a
-                        className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
-                        href={"/"}
-                    >
-                        Order Delivery
-                    </a>
-                </div>
-                <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
-                    <h1 className=" font-[540] text-[18px] text-center">HOSPITALITY<br/> COURIERS</h1>
-                    <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">We give you hours of your precious life back, with full price transparency, by supplying everything you need to run your business from our fulfillment centers to your shelves and fridges.</p>
-                    <a
-                        className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
-                        href={"/"}
-                    >
-                        Order Delivery
-                    </a>
-                </div>
-                <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
-                    <h1 className=" font-[540] text-[18px] text-center">HOSPITALITY<br/> COURIERS</h1>
-                    <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">We give you hours of your precious life back, with full price transparency, by supplying everything you need to run your business from our fulfillment centers to your shelves and fridges.</p>
-                    <a
-                        className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
-                        href={"/"}
-                    >
-                        Order Delivery
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className=" flex flex-col justify-center items-center px-[120px] mt-20">
+      <h1 className="text-[40px]">It&#39;s time to try our Services</h1>
+      <div className="grid grid-cols-4 gap-5 mt-16">
+        <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
+          <h1 className=" font-[540] text-[18px] text-center">
+            HOSPITALITY
+            <br />
+            COURIERS
+          </h1>
+          <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">
+            Need an extra hand? Give us a call we'll have someone ready to help
+            with anything to keep you running smooth!
+          </p>
+          <a
+            className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
+            href={"/"}
+          >
+            Learn More
+          </a>
         </div>
-    )
+        <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
+          <h1 className=" font-[540] text-[18px] text-center">
+            Wholesale Food
+            <br />
+            Delivery
+          </h1>
+          <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">
+            We give you hours of your precious life back, with full price
+            transparency, by supplying everything you need to run your business
+            from our fulfillment centers to your shelves and fridges.
+          </p>
+          <a
+            className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
+            href={"/"}
+          >
+            Learn More
+          </a>
+        </div>
+        <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
+          <h1 className=" font-[540] text-[18px] text-center">
+            ALCOHOL
+            <br />
+            DELIVERY
+          </h1>
+          <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">
+            We provide Alcohol Delivery for all NC Licensed Liqour
+            Establishments at an affordable rate. Simply input your order, or
+            send it for us to place. We will charge you by the case for pickup
+            and delivery.
+          </p>
+          <a
+            className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
+            href={"/"}
+          >
+            Learn More
+          </a>
+        </div>
+        <div className="shadow hover:shadow-2xl p-6 flex flex-col justify-center items-center">
+          <h1 className=" font-[540] text-[18px] text-center">
+            COMMERCIAL
+            <br />
+            CLEANING
+          </h1>
+          <p className="text-[14px] text-center text-[#0a0a0a85] font-[530]">
+            You decide how often you need us! After working in the Hospitality
+            industry for years, we were able to come up with a concept that
+            makes the customer happy. We understand the needs of the restaurant
+            and we can implement a strategy to keep clients satisfied.
+          </p>
+          <a
+            className="bg-[url('../img/btn-border_black.png')] bg-no-repeat bg-center bg-contain p-2 mt-5 text-black w-fit"
+            href={"/"}
+          >
+            Learn More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/Stats.js
+++ components/Stats.js
@@ -17,32 +17,36 @@ export default function Stats() {
           <h1 className="text-[#0A0A0A] text-[38px] font-semibold break-words">
             90%
           </h1>
-          <p className=" w-1/2 ml-10 h-fit leading-4 text-[16px] text-[#0A0A0A] break-words font-normal">
-            Timely Deliveries
+          <p className=" w-1/2 ml-20 h-fit leading-4 text-[16px] text-[#0A0A0A] break-words font-normal">
+            Timely
+            <br /> Deliveries
           </p>
         </div>
         <div className="w-6/12 flex bg-gradient-to-r from-[#a7a7a7] to-[#262628] px-5 py-1 rounded-2xl items-center">
           <h1 className="text-[#ffffff] text-[38px] font-semibold break-words">
             50K+
           </h1>
-          <p className=" w-1/2 ml-7 h-fit leading-4 text-[16px] text-[#ffffff] break-words font-normal">
-            Timely Deliveries
+          <p className=" w-1/2 ml-16 h-fit leading-4 text-[16px] text-[#ffffff] break-words font-normal">
+            Timely
+            <br /> Deliveries
           </p>
         </div>
         <div className="w-6/12 ml-16 flex bg-gradient-to-r from-[#F7BC06] to-[#ffefbf] px-5 py-1 rounded-2xl items-center">
           <h1 className="text-[#0A0A0A] text-[38px] font-semibold break-words">
             2000+
           </h1>
-          <p className=" w-1/2 ml-4 h-fit leading-4 text-[16px] text-[#0A0A0A] break-words font-normal">
-            Timely Deliveries
+          <p className=" w-1/2 ml-14 h-fit leading-4 text-[16px] text-[#0A0A0A] break-words font-normal">
+            Timely
+            <br /> Deliveries
           </p>
         </div>
         <div className="w-6/12 flex bg-gradient-to-r from-[#a7a7a7] to-[#262628] px-5 py-1 rounded-2xl items-center">
           <h1 className="text-[#ffffff] text-[38px] font-semibold break-words">
             90%
           </h1>
-          <p className=" w-1/2 ml-10 h-fit leading-4 text-[16px] text-[#ffffff] break-words font-normal">
-            Timely Deliveries
+          <p className=" w-1/2 ml-20 h-fit leading-4 text-[16px] text-[#ffffff] break-words font-normal">
+            Timely
+            <br /> Deliveries
           </p>
         </div>
       </div>
