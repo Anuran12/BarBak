0ad1c24bb95be2d39f351495ff39d7a83ec28c0d
Anuran12 <anuranarts@gmail.com>
2023-10-28 00:46:45 +0530

    about trust

--- pages/About.js
+++ pages/About.js
@@ -1,12 +1,14 @@
 import AboutHero from "@/components/AboutHero";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Trust from "@/components/Trust";
 
 export default function About() {
 	return (
         <div>
             <Header />
             <AboutHero />
+            <Trust />
             <div className="page">About Page</div>
             <Footer />
         </div>
