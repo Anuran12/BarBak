11386597c9679e706627adfa4c70d50b454f90e2
Anuran12 <anuranarts@gmail.com>
2023-10-29 12:25:06 +0530

    contact page done

--- pages/Contact.js
+++ pages/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
 	return (
         <div>
             <Header />
-            <div className="bg-[url('../img/bg_black.jpg')] bg-no-repeat bg-center bg-cover flex flex-row h-[100vh] px-[120px] pt-[200px]">
+            <div className="bg-[url('../img/bg_black.jpg')] bg-no-repeat bg-center bg-cover flex flex-row px-[120px] pt-[200px]">
                 <div className="w-1/2 text-white flex flex-col px-16">
                     <h1 className="text-[40px]">Contacts</h1>
                     <h1 className="text-[20px] mt-10">Address</h1>
@@ -29,17 +29,20 @@ export default function Contact() {
                     <Image src={ContactImg} />
                 </div>
             </div>
-            <div className="w-1/2 !bg-white/20 rounded-3xl">
-                        <div>
-                            <input className="" placeholder="Name*" />
-                            <input />
+            <div className="bg-[url('../img/bg_black.jpg')] bg-no-repeat bg-center bg-cover flex flex-row justify-center items-center px-[120px] pt-10 pb-16">
+                <div className="w-1/2 !bg-white/20 rounded-3xl flex flex-col justify-center p-10">
+                            <div className="flex flex-row justify-between mb-8">
+                                <input placeholder="Name*" className=" bg-transparent w-1/3 border-2 rounded-lg p-1 border-[#F7BC06]" />
+                                <input placeholder="Email*" className=" bg-transparent border-2 w-1/2 rounded-lg p-1 border-[#F7BC06]" />
+                            </div>
+                            <input placeholder="Subject*" className=" bg-transparent border-2 w-3/4 rounded-lg mb-8 p-1  border-[#F7BC06]"/>
+                            <textarea placeholder="Message*" type="" className=" bg-transparent border-2 text-start items-start border-[#F7BC06] p-1 h-32 rounded-lg" />
+                            <div className="w-full flex justify-center items-center">
+                                <button className="mt-5 bg-[#F7BC06] w-fit px-6 rounded-full py-2">SEND</button>
+                            </div>
                         </div>
-                        <input/>
-                        <input />
-                    </div>
-                <div className="w-full bg-none">
-                    
-                </div>
+
+            </div>
             <Footer />
         </div>
     );
