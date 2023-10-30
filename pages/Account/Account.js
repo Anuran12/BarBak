8fa0bfbc201e0d9262f9f68260c0b01d3f758ac5
Anuran12 <anuranarts@gmail.com>
2023-10-30 19:27:15 +0530

    Account page

--- pages/Account/Account.js
+++ pages/Account/Account.js
@@ -7,10 +7,12 @@ export default function Account() {
     return(
         <div>
             <Header />
-                <div className="bg-[url('../img/bg_black.jpg')] bg-no-repeat bg-center bg-cover h-[100vh]">
+                <div className="bg-[url('../img/bg_black.jpg')] bg-no-repeat bg-center bg-cover pb-10">
                     <div className=" pt-32 w-full flex flex-row">
                         <div className="w-1/2 flex flex-col justify-center items-center">
-                            <Image src={AccountImg} />
+                            <div className=" !w-[250px] h-auto">
+                                <Image src={AccountImg} />
+                            </div>
                             <h1 className="text-[40px] text-[#F7BC06] mt-3">NAME</h1>
                             <div className=" w-full flex flex-row justify-center gap-5">
                                 <a
@@ -27,35 +29,49 @@ export default function Account() {
                                 </a>
                             </div>
                         </div>
-                        <div className="w-1/2">
-                            <div className=" bg-[#F7BC06] w-fit rounded-3xl">
-                                <h1 className="text-[40px] px-20 py-6">loyalty points:-  0</h1>
+                        <div className="w-1/2 flex flex-col items-center">
+                            <div className=" bg-[#F7BC06] w-3/5 rounded-tl-[40px] rounded-br-[40px]">
+                                <h1 className="text-[40px] px-20 py-3">loyalty points:-  0</h1>
                             </div>
-                            <div>
+                            <div className=" grid grid-cols-2 w-3/5 gap-4 mt-10">
                                 <a
-                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-8 text-white w-fit"
+                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-4 text-[20px] text-white text-center w-full"
                                     href={"/"}
                                 >
                                     Order Delivery
                                 </a>
                                 <a
-                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-8 text-white w-fit"
+                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-4 text-[20px] text-white text-center w-full"
                                     href={"/"}
                                 >
                                     Order Delivery
                                 </a>
                                 <a
-                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-8 text-white w-fit"
+                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-4 text-[20px] text-white text-center w-full"
                                     href={"/"}
                                 >
                                     Order Delivery
                                 </a>
                                 <a
-                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-8 text-white w-fit"
+                                    className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-4 text-[20px] text-white text-center w-full"
                                     href={"/"}
                                 >
                                     Order Delivery
                                 </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full flex flex-row justify-center items-center">
+                        <div className="w-1/2 flex flex-col items-center">
+                            <div className="w-2/3 !bg-white/20 rounded-3xl flex flex-col justify-center p-10">
+                                
+                            </div>
+                        </div>
+                        <div className="w-1/2 flex flex-col items-center">
+                            <div className="w-2/3 flex flex-col items-center">
+                                <button className="mt-5 bg-[#F7BC06] w-2/3 px-6 rounded-full py-2">SEND</button> 
+                                <button className="mt-5 bg-[#F7BC06] w-2/3 px-6 rounded-full py-2">SEND</button> 
+                                <button className="mt-5 bg-[#F7BC06] w-2/3 px-6 rounded-full py-2">SEND</button> 
                             </div>
                         </div>
                     </div>
