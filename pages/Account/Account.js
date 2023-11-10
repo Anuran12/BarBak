b03d3efa208a75891df0b39e61d94fa986c5f364
Anuran12 <anuranarts@gmail.com>
2023-11-10 12:03:07 +0530

    edit profile button

--- pages/Account/Account.js
+++ pages/Account/Account.js
@@ -3,8 +3,10 @@ import Header from "@/components/Header";
 import Image from "next/legacy/image";
 import AccountImg from "../../img/account.png"
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Account() {
+    const [showModal, setShowModal] = useState(false);
     return(
         <div>
             <Header />
@@ -22,12 +24,12 @@ export default function Account() {
                                 >
                                     Order Delivery
                                 </a>
-                                <a
+                                <button onClick={() => setShowModal(true)}
                                     className="bg-[url('../img/btn_border_white.png')] bg-no-repeat bg-center bg-contain p-8 text-white w-fit"
                                     href={"/"}
                                 >
-                                    Order Delivery
-                                </a>
+                                    Edit Profile
+                                </button>
                             </div>
                         </div>
                         <div className="lg:w-1/2 flex flex-col items-center">
@@ -78,6 +80,134 @@ export default function Account() {
                     </div>
                 </div>
             <Footer />
+            {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[url('../img/wood.jpg')] bg-no-repeat bg-center bg-cover outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold text-[#F7BC06]">
+                    Edit Profile
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      x
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-2 flex-auto">
+                <div className="rounded-3xl flex flex-col justify-center">
+                    <div className="flex flex-row justify-between gap-2">
+                        <div class="">
+                            <label class="block mb-2 text-[16px] text-[#F7BC06]">
+                            Name
+                            </label>
+                            <input
+                            type="text"
+                            placeholder="Name*"
+                            class="block w-full px-5 py-1 mt-2 text-black font-bold placeholder-gray-300 bg-white/40 border border-gray-200 rounded-lg focus:border-[#F7BC06] focus:ring-[#f7bb06bb] focus:outline-none focus:ring focus:ring-opacity-40"
+                            />
+                        </div>
+                        <div class="">
+                            <label class="block mb-2 text-[16px] text-[#F7BC06]">
+                            Mobile Number
+                            </label>
+                            <input
+                            type="number"
+                            placeholder="Mobile Number*"
+                            class="block w-full px-5 py-1 mt-2 text-black font-bold placeholder-gray-300 bg-white/40 border border-gray-200 rounded-lg focus:border-[#F7BC06] focus:ring-[#f7bb06bb] focus:outline-none focus:ring focus:ring-opacity-40"
+                            />
+                        </div>
+
+                    </div>
+              <div class="mt-4">
+                <label class="block mb-2 text-[16px] text-[#F7BC06]">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  placeholder="Address*"
+                  class="block w-full px-5 py-1 mt-2 text-black font-bold placeholder-gray-300 bg-white/40 border border-gray-200 rounded-lg focus:border-[#F7BC06] focus:ring-[#f7bb06bb] focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+              <div className="flex flex-row justify-between gap-2">
+              <div class="mt-4">
+                <label class="block mb-2 text-[16px] text-[#F7BC06]">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="Email*"
+                  class="block w-full px-5 py-1 mt-2 text-black font-bold placeholder-gray-300 bg-white/40 border border-gray-200 rounded-lg focus:border-[#F7BC06] focus:ring-[#f7bb06bb] focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+              <div class="mt-4">
+                <label class="block mb-2 text-[16px] text-[#F7BC06]">
+                  Old Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password*"
+                  class="block w-full px-5 py-1 mt-2 text-black font-bold placeholder-gray-300 bg-white/40 border border-gray-200 rounded-lg focus:border-[#F7BC06] focus:ring-[#f7bb06bb] focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+              </div>
+              <div className="flex flex-row justify-between gap-2">
+              <div class="mt-4">
+                <label class="block mb-2 text-[16px] text-[#F7BC06]">
+                  New Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Confirm Password*"
+                  class="block w-full px-5 py-1 mt-2 text-black font-bold placeholder-gray-300 bg-white/40 border border-gray-200 rounded-lg focus:border-[#F7BC06] focus:ring-[#f7bb06bb] focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+              <div class="mt-4">
+                <label class="block mb-2 text-[16px] text-[#F7BC06]">
+                  Confirm New Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Confirm Password*"
+                  class="block w-full px-5 py-1 mt-2 text-black font-bold placeholder-gray-300 bg-white/40 border border-gray-200 rounded-lg focus:border-[#F7BC06] focus:ring-[#f7bb06bb] focus:outline-none focus:ring focus:ring-opacity-40"
+                />
+              </div>
+              </div>
+              
+            </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-[#F7BC06] text-black font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
         </div>
     )
 }
