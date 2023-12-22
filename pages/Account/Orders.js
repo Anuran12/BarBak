9909f38d1759819108fbce74da9eb8e95a464b95
Anuran12 <anuranarts@gmail.com>
2023-12-23 05:11:58 +0530

    fix order page

--- pages/Account/Orders.js
+++ pages/Account/Orders.js
@@ -26,7 +26,9 @@ export default function Orders(){
     return(
         <div>
             <Header />
-             <div className="bg-[url('../img/bg_black.jpg')] bg-no-repeat bg-center bg-cover pt-28 pb-16 lg:px-[120px]">
+            <div className="bg-[url('../img/bg_black.jpg')] bg-no-repeat bg-center bg-cover w-full h-full fixed -z-30"></div>
+             <div className=" pt-28 pb-16 lg:px-[120px]">
+
         <h1 className="text-[40px] text-[#F7BC06] text-center">My Orders</h1>
         <div className="flex flex-col gap-4">
           {[...Array(5).keys()].map((index) => (
@@ -65,8 +67,8 @@ export default function Orders(){
                                     <svg width="40px" height="40px" viewBox="0 0 1024 1024" fill="#F7BC06" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M642.174 504.594c7.99 7.241 7.897 17.58-0.334 24.782L332.62 799.945c-8.867 7.759-9.766 21.236-2.007 30.103 7.758 8.867 21.236 9.766 30.103 2.007l309.221-270.569c27.429-24 27.792-64.127 0.89-88.507L360.992 192.192c-8.73-7.912-22.221-7.248-30.133 1.482-7.912 8.73-7.248 22.222 1.482 30.134l309.833 280.786z" fill="" /></svg>
                             </div>
 
-                {orderStates[index] ? (
-                    <div className="col-span-5">
+                
+                    <div className={`col-span-5 dropdown ${orderStates[index] ? "dropdownOpen" : ""}`}>
                         <Link className=" col-span-5 mb-5" href={`/Account/Tracking`}>
                         {/* ... rest of your code ... */}
                         <div href={'/Account/Tracking'} className="grid lg:grid-cols-5 grid-cols-4 bg-white/20 hover:bg-black/20 rounded-t-xl p-2 items-center">
@@ -120,7 +122,7 @@ export default function Orders(){
                                     <svg width="40px" height="40px" viewBox="0 0 1024 1024" fill="#F7BC06" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M642.174 504.594c7.99 7.241 7.897 17.58-0.334 24.782L332.62 799.945c-8.867 7.759-9.766 21.236-2.007 30.103 7.758 8.867 21.236 9.766 30.103 2.007l309.221-270.569c27.429-24 27.792-64.127 0.89-88.507L360.992 192.192c-8.73-7.912-22.221-7.248-30.133 1.482-7.912 8.73-7.248 22.222 1.482 30.134l309.833 280.786z" fill="" /></svg>
                             </div>
                         </div>
-                        <div className="grid grid-cols-4">
+                        <div className="grid grid-cols-4 bg-white/20">
                             <div className="text-white text-center p-2 Tracking_box bg-red-500/60 border-white">
                             </div>
                             <div className="text-white text-center  p-2 Tracking_box bg-red-500/60 border-white">
@@ -133,9 +135,6 @@ export default function Orders(){
                     </Link>
                     </div>
                     
-                ) : (
-                  <div></div>
-                )}
               </div>
             </div>
           ))}
